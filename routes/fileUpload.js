5c42368f6e7422b840a957f3a5de70f41e9d840a
PAwel_Z97 <pawelzych97@gmail.com>
2020-11-10 16:19:52 +0100

    xee fix, no xml allowed

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -76,33 +76,8 @@ function checkFileType ({ file }, res, next) {
 
 function handleXmlUpload ({ file }, res, next) {
   if (utils.endsWith(file.originalname.toLowerCase(), '.xml')) {
-    utils.solveIf(challenges.deprecatedInterfaceChallenge, () => { return true })
-    if (file.buffer && !utils.disableOnContainerEnv()) { // XXE attacks in Docker/Heroku containers regularly cause "segfault" crashes
-      const data = file.buffer.toString()
-      try {
-        const sandbox = { libxml, data }
-        vm.createContext(sandbox)
-        const xmlDoc = vm.runInContext('libxml.parseXml(data, { noblanks: true, noent: true, nocdata: true })', sandbox, { timeout: 2000 })
-        const xmlString = xmlDoc.toString(false)
-        utils.solveIf(challenges.xxeFileDisclosureChallenge, () => { return (matchesSystemIniFile(xmlString) || matchesEtcPasswdFile(xmlString)) })
-        res.status(410)
-        next(new Error('B2B customer complaints via file upload have been deprecated for security reasons: ' + utils.trunc(xmlString, 400) + ' (' + file.originalname + ')'))
-      } catch (err) {
-        if (utils.contains(err.message, 'Script execution timed out')) {
-          if (utils.notSolved(challenges.xxeDosChallenge)) {
-            utils.solve(challenges.xxeDosChallenge)
-          }
-          res.status(503)
-          next(new Error('Sorry, we are temporarily not available! Please try again later.'))
-        } else {
-          res.status(410)
-          next(new Error('B2B customer complaints via file upload have been deprecated for security reasons: ' + err.message + ' (' + file.originalname + ')'))
-        }
-      }
-    } else {
-      res.status(410)
-      next(new Error('B2B customer complaints via file upload have been deprecated for security reasons (' + file.originalname + ')'))
-    }
+    res.status(410)
+    next(new Error('B2B customer complaints via file upload have been deprecated for security reasons (' + file.originalname + ')'))
   }
   res.status(204).end()
 }
